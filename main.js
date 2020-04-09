a1017f039bf51cdb2362b854bb3d3ce76aea8b47
Ubuntu <ubuntu@ip-172-31-80-234.ec2.internal>
2020-04-09 03:11:38 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// function clicked(){
+//     var data;
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", "https://www.hatchways.io/api/assessment/students",true);
+//     xhr.send();
+//     xhr.onreadystatechange = function() {
+//         if (this.status === 200) {
+//         console.log(JSON.parse(this.responseText));
+
+//             // document.getElementById("data").innerHTML = this.responseText;
+//         //   data = JSON.parse;
+//         }
+
+// };
+// }
+
+
+
+
+
